93159b6246915839093f4d8965e643b0d000ef72
Antoine Bartuccio <klmp200@klmp200.net>
2016-06-07 00:16:57 +0200

    Ajout de template de base et ajout d'une mini nav-bar

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@
 # @Author: klmp200
 # @Date:   2016-06-04 23:24:06
 # @Last Modified by:   klmp200
-# @Last Modified time: 2016-06-06 16:33:05
+# @Last Modified time: 2016-06-06 23:23:21
 
 from django.conf.urls import patterns, url
 
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,34 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 from blog.models import Article, Page
 
 
 def blog(request):
 
     articles = Article.objects.exclude(published=False).order_by('-date')
-    return render(request, 'blog/blog.html', {'articles': articles})
+    return render(request, 'blog/blog.html', {'articles': articles, 'nav': get_nav()})
 
 
 def article(request, slug):
 
     article = get_object_or_404(Article, slug=slug, published=True)
 
-    return render(request, 'blog/article.html', {'article': article})
+    return render(request, 'blog/article.html', {'article': article, 'nav': get_nav()})
 
 
 def page(request, slug):
 
-    content = get_object_or_404(Page, slug, published=True)
+    content = get_object_or_404(Page, slug=slug, published=True)
 
-    return render(request, 'blog/page.html', {'content': content})
+    return render(request, 'blog/page.html', {'content': content, 'nav': get_nav()})
+
+
+def get_nav():
+
+    nav = {
+        'pages': Page.objects.exclude(published=False).order_by('order'),
+        'path': reverse('blog')
+    }
+
+    return nav
