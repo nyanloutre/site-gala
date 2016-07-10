dd4c0193c96dcc4249add4dcfd5ad0d95f505506
klmp200 <klmp200@klmp200.net>
2016-07-10 16:52:04 +0200

    On essaye de faire un timer joli (c'est pas encore ça) et on tente une faiblarde barre de navigation

--- blog/views.py
+++ blog/views.py
@@ -7,28 +7,32 @@ from blog.models import Article, Page
 def blog(request):
 
     articles = Article.objects.exclude(published=False).order_by('-date')
-    return render(request, 'blog/blog.html', {'articles': articles, 'nav': get_nav()})
+    return render(request, 'blog/blog.html',
+                  {'articles': articles, 'nav': get_nav(request)})
 
 
 def article(request, slug):
 
     article = get_object_or_404(Article, slug=slug, published=True)
 
-    return render(request, 'blog/article.html', {'article': article, 'nav': get_nav()})
+    return render(request, 'blog/article.html',
+                  {'article': article, 'nav': get_nav(request)})
 
 
 def page(request, slug):
 
     content = get_object_or_404(Page, slug=slug, published=True)
 
-    return render(request, 'blog/page.html', {'content': content, 'nav': get_nav()})
+    return render(request, 'blog/page.html',
+                  {'content': content, 'nav': get_nav(request)})
 
 
-def get_nav():
+def get_nav(request):
 
     nav = {
         'pages': Page.objects.exclude(published=False).order_by('order'),
-        'path': reverse('blog')
+        'index': reverse('blog'),
+        'path': request.path
     }
 
     return nav
